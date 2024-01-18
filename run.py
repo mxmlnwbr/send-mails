61dbe1e289f6bd71b91628ce2cd33ba6dff35f17
mxmlnwbr <maximilian.weber@bluewin.ch>
2024-01-18 22:50:00 +0100

    updated

--- run.py
+++ run.py
@@ -10,13 +10,17 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
+attach_files = True
+add_image_to_body = False
+
 load_dotenv()
 
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 
-df = pd.read_excel("/Users/mw/Documents/send-mails/input.xlsx", sheet_name=0)
-email_adresses = df["E-Mail"].tolist()
+# df = pd.read_excel("/Users/mw/Documents/send-mails/input.xlsx", sheet_name=0)
+# email_adresses = df["E-Mail"].tolist()
+email_adresses = ["maximilian.weber@bluewin.ch", "mxjweber@gmail.com"]
 
 FROM = os.getenv("GMAIL_ACCOUNT")
 PASSWORD = os.getenv("GMAIL_PASSWORD")
@@ -32,30 +36,7 @@ for toaddr in tqdm(email_adresses):
 
     tqdm.write(f"Number {counter}: {toaddr}")
 
-    # vorname = df["Vorname:"].tolist()[counter - 1].capitalize()
-    # if df["Geschlecht:"].tolist()[counter - 1] == "Weiblich":
-    #     Anrede = "Liebe"
-    # else:
-    #     Anrede = "Lieber"
-
-    # subject = "Weihnachtsgrüsse von der Flusswelle Muota!"
-    # body = f"""
-    # <html>
-    #     <body>
-    #         <p>{Anrede} {vorname},</p>
-    #         <p>wir hoffen, dass diese Nachricht dich in bester Gesundheit erreicht und du eine schöne Vorweihnachtszeit hast. Wir wünschen frohe Weihnachten und ein strahlendes neues Jahr! Möge diese Zeit des Jahres mit Freude, Wärme und unvergesslichen Momenten mit deinen Liebsten gefüllt sein.</p>
-    #         <p>Es gibt Updates bezüglich der Flusswelle: Der geplante Abriss der Muota Flusswelle wird nach wie vor vorangetrieben (EBS & Bezirk Schwyz). Allerdings konnten wir dank der Unterstützung dieses Vereins und euch als geschätzte Mitglieder Argumente vorbringen, die zu einem Gespräch mit den Projektverantwortlichen geführt haben. Der nächste Schritt besteht nun darin, auszuarbeiten, wie eine "neue" Welle im Rahmen des Projekts realisiert werden könnte.</p>
-    #         <p>In diesem Sinne möchten wir euch herzlich danken. Gemeinsam machen wir die Flusswelle Muota zu einem Ort der Begeisterung und des Austauschs für Wassersportliebhaber. Wir freuen uns auf ein aufregendes neues Jahr voller Wellen und spannender Momente!</p>
-    #         <p>Frohe Feiertage und einen fantastischen Start ins neue Jahr!</p>
-    #         <p>P.S. Anbei findest du unsere Stellungnahme zum Vorprojekt des Bezirks Schwyz.</p>
-    #         <p>Weihnachtliche Grüsse 🎅,<br>
-    #         Flusswelle 🌊 Muota</p>
-    #         <br>
-    #         <img src="cid:image1">
-    #     </body>
-    # </html>
-    # """
-    subject = "Rücknahme zu viel gekaufter Tickets - RigiBeats 2024"
+    subject = "RigiBeats 2024 - RigiBahn Ticket"
     body = """
     <html>
         <body>
@@ -67,6 +48,8 @@ for toaddr in tqdm(email_adresses):
             <p>Wir bedanken uns für dein Verständnis und deine Mitarbeit in dieser Angelegenheit. Unser Ziel ist es, sicherzustellen, dass jeder Gast die bestmögliche Erfahrung bei unserem Event hat. </p>
             <p>Wir freuen uns schon riesig darauf, dich bald auf der Rigi zu sehen und gemeinsam eine grossartige Zeit zu haben! </p>
             <p>Dein RigiBeats Team ❤️</p>
+            <br>
+            <img src="cid:image1">
         </body>
     </html>
     """
@@ -78,32 +61,34 @@ for toaddr in tqdm(email_adresses):
     msg.attach(MIMEText(body, "html"))
 
     # ADD IMAGE TO BODY
-    # with open("/Users/mw/Documents/send-mails/img.jpeg", "rb") as fp:
-    #     img = MIMEImage(fp.read())
-    #     img.add_header("Content-ID", "<image1>")
-    #     msg.attach(img)
+    if add_image_to_body:
+        with open("/Users/mw/Documents/send-mails/img.jpeg", "rb") as fp:
+            img = MIMEImage(fp.read())
+            img.add_header("Content-ID", "<image1>")
+            msg.attach(img)
 
     # ATTACH FILE TO EMAIL
-    # open the file to be sent
-    # filename = "Stellungnahme_Vorprojekt_Bezirk_Schwyz.pdf"
-    # attachment = open(
-    #     "/Users/mw/Documents/send-mails/Stellungnahme_Vorprojekt_Bezirk_Schwyz.pdf",
-    #     "rb",
-    # )
-    #
-    # instance of MIMEBase and named as p
-    # p = MIMEBase("application", "octet-stream")
-    #
-    # To change the payload into encoded form
-    # p.set_payload((attachment).read())
-    #
-    # encode into base64
-    # encoders.encode_base64(p)
-    #
-    # p.add_header("Content-Disposition", "attachment; filename= %s" % filename)
-    #
-    # attach the instance 'p' to instance 'msg'
-    # msg.attach(p)
+    if attach_files:
+        # open the file to be sent
+        filename = f"dummy{counter}.pdf"
+        attachment = open(
+            f"/Users/mw/Documents/send-mails/files/dummy{counter}.pdf",
+            "rb",
+        )
+
+        # instance of MIMEBase and named as p
+        p = MIMEBase("application", "octet-stream")
+
+        # To change the payload into encoded form
+        p.set_payload((attachment).read())
+
+        # encode into base64
+        encoders.encode_base64(p)
+
+        p.add_header("Content-Disposition", "attachment; filename= %s" % filename)
+
+        # attach the instance 'p' to instance 'msg'
+        msg.attach(p)
 
     # creates SMTP session
     s = smtplib.SMTP("smtp.gmail.com", 587)
